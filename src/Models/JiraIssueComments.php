547f8a4f0c6e67f79b358c5b435045710fcd329f
Miss Lv <lv@devadmin.com>
2017-03-07 12:32:23 +0000

    Handle issues with no comments

--- src/Models/JiraIssueComments.php
+++ src/Models/JiraIssueComments.php
@@ -55,13 +55,9 @@ class JiraIssueComments
     {
         $issueCommentsData = new self;
 
-        if (!$data) {
+        if (!$data || empty($data['comments'])) {
             return $issueCommentsData;
         }
-
-        /*if (empty($data['comments'])) {
-            throw new JiraWebhookDataException('JIRA issue comments does not exist!');
-        }*/
 
         foreach ($data['comments'] as $key => $comment) {
             $issueCommentsData->setComment($key, $comment);
