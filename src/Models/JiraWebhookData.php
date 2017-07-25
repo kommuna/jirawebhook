93d544aa33e3cca22f04ce409067a2ad58dfccbc
Chewbacca96 <lebedev582@gmail.com>
2017-07-25 11:20:09 +0300

    [TL-81] Comment fix

--- src/Models/JiraWebhookData.php
+++ src/Models/JiraWebhookData.php
@@ -126,7 +126,6 @@ class JiraWebhookData
      */
     public function isIssueCommented()
     {
-        //return $this->issueEvent === 'issue_commented';
         return array_key_exists('comment', $this->rawData);
     }
 
